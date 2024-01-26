11ff4d6299ec702773a38df0deb34873ef2f703b
Armanc S <armancdev@fastmail.com>
2024-01-27 01:04:53 +0300

    Update sets

--- collections/sets.go
+++ collections/sets.go
@@ -1,24 +1,41 @@
 package collections
 
+// Set is a generic set implementation using a map with empty struct as a placeholder value
 type Set[T comparable] map[T]struct{}
 
-func (set Set[T]) Add(value T) {
-	set[value] = struct{}{}
+// Add adds an element to the set
+func (s Set[T]) Add(item T) {
+	s[item] = struct{}{}
 }
 
-func (set Set[T]) Remove(value T) {
-	delete(set, value)
+// Remove removes an element from the set
+func (s Set[T]) Remove(item T) {
+	delete(s, item)
 }
 
-func (set Set[T]) Has(value T) bool {
-	_, exist := set[value]
-	return exist
+// Contains checks if the set contains a specific element
+func (s Set[T]) Contains(item T) bool {
+	_, exists := s[item]
+	return exists
 }
 
-func (set Set[T]) ToSlice() []T {
-	slice := make([]T, 0, len(set))
-	for value := range set {
-		slice = append(slice, value)
+// Has checks if the set contains a specific element
+//
+// Deprecated: Use Contains instead. Has will be removed in the next release.
+func (s Set[T]) Has(item T) bool {
+	return s.Contains(item)
+}
+
+// Size returns the number of elements in the set
+func (s Set[T]) Size() int {
+	return len(s)
+}
+
+// ToSlice returns a slice of all elements in the set
+func (s Set[T]) ToSlice() []T {
+	slice := make([]T, 0, len(s))
+	for item := range s {
+		slice = append(slice, item)
 	}
 	return slice
 }
